269d7e0446ee5c0c6644fe7e70f1130e9c6ccee9
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-05-20 15:13:17 -0400

    Changed logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     return (
         <div className="navbar">
             
-            <Link to="/" className="navbar__title"><img src="Workout Lab Logo 2.png" alt="Workout Lab Logo" width="30%"/></Link>
+            <Link to="/" className="navbar__title"><img src="Workout Lab Logo.png" alt="Workout Lab Logo" width="30%"/></Link>
             <div className="navbar__options">
                 <Link to="/workout-creator" className="navbar__options">Workout Creator</Link>
                 <Link to="/workouts" className="navbar__options">Workouts</Link>
