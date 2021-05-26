36f83cb0318af4b93ac16d5d8d9331f0ee2aaae9
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-05-26 00:52:00 -0400

    Added to homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import './Homepage.css';
 
 function Homepage() {
     return (
-        <div>
-            <p>The Workout Center</p>
-            <p>Discover new exercises</p>
-            <p>Build custom workouts</p>
-            <p>Target specific muscles</p>
+        <div className="homepage">
+            <h1>Welcome to the Workout Lab</h1>
+            <p>The center for achieving your workout goals.</p>
+            <h3>Discover new exercises</h3>
+            <h3>Build custom workouts</h3>
+            <h3>Target specific muscles</h3>
         </div>
     )
 }
