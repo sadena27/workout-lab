9b065fa58197e2b6b34f26b7084ae70cb2801fd1
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-05-31 01:28:19 -0400

    Used api to display first 20 exercise images

--- src/Exercises.js
+++ src/Exercises.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Model from 'react-body-highlighter';
 import './Exercises.css';
-
+import axios from 'axios';
 
 const exercises = [
     {
@@ -75,13 +75,34 @@ const exercises = [
 ];
 
 function Exercises() {
+    const [data, setData] = useState([]);
+    const [exercises, setExercises] = useState([{name: '', id: ''}]);
+
+    useEffect(() => {
+        async function getData() {
+            const response = await axios.get('https://wger.de/api/v2/exerciseimage/?is_main=True/?language=2');
+            setData(response.data.results);
+        }
+        async function getExercises() {
+            const response = await axios.get('https://wger.de/api/v2/exercise/?limit=20&offset=360');
+            setExercises(response.data.results);
+        }
+        getData();
+        getExercises();
+     }, [])
+
+
+    let exerciseName
+
     return (
         <div className="exercises">
-            {exercises.map((exercise) => (
+            {data.map((exercise) => (
                 <div className="exercises__box">
-                    <a href={exercise.link} target="_blank">{exercise.info.name}</a>
+                    {exercises.length == 0 ? (<a href={exercise.image} target="_blank">{"test"}</a>) :
+                    (<a href={exercise.image} target="_blank">{"test2"}</a>)}
                     <p/>
-                    <div className="exercises__box__bodies">
+                    <img src={exercise.image} alt="exercise image"/>
+                    {/* <div className="exercises__box__bodies">
                         <Model
                             data={exercise.info}
                             style={{ width: '70px'}}
@@ -94,7 +115,7 @@ function Exercises() {
                             type="posterior"
                             highlightedColors={['#87bfff','#3f8efc']}
                         />
-                    </div>
+                    </div> */}
                 </div>
             ))}
         </div>
