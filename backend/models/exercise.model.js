0ab1cd0aa6910ea69b72e375ffa40654c1c170fa
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-06-12 19:22:24 -0400

    Created backend and installed packages

---
+++ backend/models/exercise.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    username: {type: String, required: true,},
+    exerciseName: {type: String, required: true,},
+    sets: {type: Number, required: true,},
+    reps: {type: Number, required: true},
+}, {
+    timestamps: true,
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+module.exports = Exercise;
