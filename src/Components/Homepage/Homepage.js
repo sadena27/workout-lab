7c8c30b2990c4e0580d9dda895ae5413a71104a7
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-08-02 22:59:29 -0400

    Added learn more section with sample logs

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -8,12 +8,12 @@ function Homepage() {
             <div className="intro">
                 <div className="intro__info">
                     <h1>WELCOME TO THE WORKOUT LAB.</h1>
-                    <a href="#exploreBox" className="intro__button">Learn More</a>
+                    <a href="#learnMore" className="intro__button">Learn More</a>
                     <Link to="/login" className="intro__button">Login</Link>
                 </div>
                 <img src="homepage_background.jpg" alt="man deadlifting"></img>
             </div>
-            <div id="exploreBox">
+            <div className="exploreBox">
                 <div className="exploreBox__card">
                     <div className="exploreBox__card__exercise">
                         <Link to="/exercises" className="exploreCardText">EXPLORE EXERCISES</Link>
@@ -23,6 +23,14 @@ function Homepage() {
                     <div className="exploreBox__card__workout">
                         <Link to="/workout-tracker" className="exploreCardText">TRACK YOUR WORKOUTS</Link>
                     </div>
+                </div>
+            </div>
+            <div id="learnMore">
+                <div className="showcase">
+                    <h2>Sample workout log: </h2>
+                    <img src="workout_log.png" alt="workout log example"></img>
+                    <h2>Sample exercise log: </h2>
+                    <img src="exercise_log.png" alt="exercise log example"></img>
                 </div>
             </div>
         </div>
